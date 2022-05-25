dc86a7accb091c4309e2c7eee33fd81cec2c3b0d
shawnduong <shawnduong@protonmail.com>
2022-05-24 17:26:51 -0700

    Implemented root percentage calculation.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,47 @@
 /* Set the packages count. */
-$("#count").text(data["packages"].length);
+$("#pkgs-count").text(data["packages"].length);
+
+/* Calculate the partition usage percentage. */
+$("#part-percent").text((100 * data["used"]/data["total"]).toFixed(2) + "%");
+
+/* Generate the partition usage chart. */
+const partChart = new Chart(
+
+	document.getElementById("part-chart"),
+	{
+		type: "pie",
+		data:
+		{
+			labels: ["Used", "Free"],
+			datasets:
+			[
+				{
+					label: "Data",
+					data:
+					[
+						(data["used"]/1073741824).toFixed(2),
+						((data["total"] - data["used"])/1073741824).toFixed(2),
+					],
+					backgroundColor: ["#FF0000", "#00FFFF"],
+				},
+			],
+		},
+		options:
+		{
+			plugins:
+			{
+				legend:
+				{
+					display: false,
+				},
+				tooltip:
+				{
+					callbacks:
+					{
+						label: (item) => ` ${item.formattedValue} GiB`,
+					},
+				},
+			},
+		},
+	}
+)
